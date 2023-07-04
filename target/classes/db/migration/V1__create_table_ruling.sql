c5e5b6748580213a69341727dfb529ed41300fe6
Wellik Perroni <welliksp@gmail.com>
2023-07-04 17:56:17 -0300

    delete target dir

--- target/classes/db/migration/V1__create_table_ruling.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists ruling
-(
-    id         bigint(20)  not null auto_increment,
-    name       varchar(50) not null,
-    created_at timestamp   not null,
-    created_by varchar(50) not null,
-    primary key (id)
-);
--- target/classes/db/migration/V2__create_table_parameter.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists parameter
-(
-    id         bigint(20)  not null auto_increment,
-    value       varchar(50) not null,
-    primary key (id)
-);
